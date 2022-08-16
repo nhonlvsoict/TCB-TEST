c467d07242938d52c54df48bf5954eb335727d06
nhonlv <nhonlv.soict@gmail.com>
2022-08-16 16:29:28 +0700

    upload test prj

---
+++ TCB-TEST_UnitTests/Domain/Repositories/PoolRepositoryTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TCB_TEST.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TCB_TEST.Domain.Models;
+using System.Threading.Tasks;
+
+namespace TCB_TEST.Domain.Repositories.Tests
+{
+    [TestClass()]
+    public class PoolRepositoryTests
+    {
+        [TestMethod()]
+        public async Task SavePoolListTest()
+        {
+            IPoolRepository poolRe = new PoolRepository();
+            double[] vlLst = { 2, 3, 4 };
+            Pool pool = new Pool(123, new List<double>(vlLst));
+            List<Pool> pooLst = new List<Pool>();
+            pooLst.Add(pool);
+            var r = await poolRe.SavePoolList(pooLst);
+            Assert.IsNotNull(r);
+        }
+    }
+}
---
+++ TCB-TEST_UnitTests/Domain/Services/PoolServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TCB_TEST.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Sdk;
+using Moq;
+using TCB_TEST.Domain.Models;
+using System.Threading.Tasks;
+using TCB_TEST.Domain.Repositories;
+
+namespace TCB_TEST.Domain.Services.Tests
+{
+   
+
+
+    [TestClass()]
+    public class PoolServiceTests
+    {
+        /*private IPoolRepository _poolRespository = new PoolRepository();*/
+
+        Mock<IPoolRepository> _poolRespository = new Mock<IPoolRepository>();
+
+        /*public PoolServiceTests(IPoolService poolService)
+        {
+            _poolService = poolService;
+        }*/
+
+        /*[TestMethod()]
+        public async Task GetPoolTest()
+        {
+            Assert.Fail();
+        }*/
+
+        [TestMethod()]
+        public async Task AppendOrInsertTest()
+        {
+            IPoolService _poolService = new PoolService(_poolRespository.Object);
+            double[] vlLst = { 2, 3, 4 };
+            Pool pool = new Pool(123, new List<double>(vlLst));
+
+            _poolRespository.Setup(p => p.SavePoolList(It.IsAny<List<Pool>>())).ReturnsAsync(true);
+            _poolRespository.Setup(p => p.GetPool(It.IsAny<int>())).ReturnsAsync((Pool) null);
+            _poolRespository.Setup((p) =>  p.GetPoolList()).ReturnsAsync(new List<Pool>());
+            
+            
+            Assert.IsFalse(await _poolService.AppendOrInsert(pool));
+            /*Assert.Fail();*/
+        }
+    }
+}
